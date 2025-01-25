032d2b3927c436505051865049576cdee2c03667
quettabit <dwarak@s2.dev>
2025-01-24 19:30:15 -0700

    initial commit

--- src/streamstore/__init__.py
+++ src/streamstore/__init__.py
@@ -1,4 +1,11 @@
-__all__ = ["S2", "S2Error", "streamstore.schemas", "streamstore.utils"]
+__all__ = [
+    "S2",
+    "Basin",
+    "Stream",
+    "S2Error",
+    "streamstore.schemas",
+    "streamstore.utils",
+]
 
-from streamstore._client import S2
+from streamstore._client import S2, Basin, Stream
 from streamstore._exceptions import S2Error
