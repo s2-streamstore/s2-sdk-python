262a5e217ed4dca80da53579bba9e199b72a2ecb
quettabit <27509167+quettabit@users.noreply.github.com>
2025-08-06 21:41:37 -0600

    feat: add access token APIs (#86)

--- src/streamstore/_mappers.py
+++ src/streamstore/_mappers.py
@@ -5,14 +5,21 @@ from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
 
 import streamstore._lib.s2.v1alpha.s2_pb2 as msgs
 from streamstore.schemas import (
+    AccessTokenInfo,
+    AccessTokenScope,
     AppendInput,
     AppendOutput,
     BasinConfig,
     BasinInfo,
     BasinScope,
     BasinState,
+    Operation,
+    OperationGroupPermissions,
+    Permission,
     ReadLimit,
     Record,
+    ResourceMatchOp,
+    ResourceMatchRule,
     SequencedRecord,
     StorageClass,
     StreamConfig,
@@ -131,6 +138,102 @@ def sequenced_records_schema(
         SequencedRecord(sr.seq_num, sr.body, [(h.name, h.value) for h in sr.headers])
         for sr in batch.records
     ]
+
+
+def access_token_info_message(
+    id: str, scope: AccessTokenScope, auto_prefix_streams: bool, expires_at: int | None
+) -> msgs.AccessTokenInfo:
+    def resource_set(rule: ResourceMatchRule | None) -> msgs.ResourceSet | None:
+        if rule is None:
+            return None
+        match rule.match_op:
+            case ResourceMatchOp.EXACT:
+                return msgs.ResourceSet(exact=rule.value)
+            case ResourceMatchOp.PREFIX:
+                return msgs.ResourceSet(prefix=rule.value)
+            case _:
+                raise ValueError(
+                    "ResourceMatchOp doesn't match any of the expected values"
+                )
+
+    def permissions(perm: Permission) -> msgs.ReadWritePermissions:
+        read = False
+        write = False
+        match perm:
+            case Permission.UNSPECIFIED:
+                pass
+            case Permission.READ:
+                read = True
+            case Permission.WRITE:
+                write = True
+            case Permission.READ_WRITE:
+                read = True
+                write = True
+        return msgs.ReadWritePermissions(read, write)
+
+    def permitted_op_groups(
+        op_group_perms: OperationGroupPermissions | None,
+    ) -> msgs.PermittedOperationGroups | None:
+        if op_group_perms is None:
+            return None
+        return msgs.PermittedOperationGroups(
+            account=permissions(op_group_perms.account),
+            basin=permissions(op_group_perms.basin),
+            stream=permissions(op_group_perms.stream),
+        )
+
+    return msgs.AccessTokenInfo(
+        id=id,
+        expires_at=expires_at,
+        auto_prefix_streams=auto_prefix_streams,
+        scope=msgs.AccessTokenScope(
+            basins=resource_set(scope.basins),
+            streams=resource_set(scope.streams),
+            tokens=resource_set(scope.access_tokens),
+            op_groups=permitted_op_groups(scope.op_group_perms),
+            ops=(msgs.Operation(op.value) for op in scope.ops) if scope.ops else None,
+        ),
+    )
+
+
+def access_token_info_schema(info: msgs.AccessTokenInfo) -> AccessTokenInfo:
+    def resource_match_rule(resource_set: msgs.ResourceSet) -> ResourceMatchRule:
+        match resource_set.WhichOneof("matching"):
+            case "exact":
+                return ResourceMatchRule(ResourceMatchOp.EXACT, resource_set.exact)
+            case "prefix":
+                return ResourceMatchRule(ResourceMatchOp.PREFIX, resource_set.prefix)
+            case _:
+                raise RuntimeError(
+                    "ResourceSet matching doesn't match any of the expected values"
+                )
+
+    def permission(perms: msgs.ReadWritePermissions) -> Permission:
+        if perms.read and perms.write:
+            return Permission.READ_WRITE
+        elif perms.read:
+            return Permission.READ
+        elif perms.write:
+            return Permission.WRITE
+        else:
+            return Permission.UNSPECIFIED
+
+    return AccessTokenInfo(
+        id=info.id,
+        scope=AccessTokenScope(
+            basins=resource_match_rule(info.scope.basins),
+            streams=resource_match_rule(info.scope.streams),
+            access_tokens=resource_match_rule(info.scope.tokens),
+            op_group_perms=OperationGroupPermissions(
+                account=permission(info.scope.op_groups.account),
+                basin=permission(info.scope.op_groups.basin),
+                stream=permission(info.scope.op_groups.stream),
+            ),
+            ops=[Operation(op) for op in info.scope.ops],
+        ),
+        auto_prefix_streams=info.auto_prefix_streams,
+        expires_at=info.expires_at if info.HasField("expires_at") else None,
+    )
 
 
 def _not_a_command_record(
